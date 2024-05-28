2d1eb0af085fe44df14c7b6aed5e6256c6e8c0ab
Bongkan <bongkan.jj@gmail.com>
2024-05-28 15:52:57 +0700

    ex3

--- exercise_02/App.jsx
+++ exercise_02/App.jsx
@@ -2,8 +2,51 @@ import React from "react";
 
 const App = () => {
   const temperature = 25;
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
 
-	return <>{/* code here */};</>;
+const Header = () => {
+  return (
+    <div>
+      <button>ON / OFF</button>
+      <span>Current Temperature: 25 °C</span>
+    </div>
+  );
+};
+
+const Content = () => {
+  return (
+    <div>
+      <Temperature />
+    </div>
+  );
+};
+
+const Temperature = () => {
+  return (
+    <div>
+      25 °C
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div>
+      <button>
+        Down
+      </button>
+      <button >
+        Up
+      </button>
+    </div>
+  );
 };
 
 export default App;
--- exercise_03/App.jsx
+++ exercise_03/App.jsx
@@ -1,9 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
 	const temperature = 25;
 
-	return <>{/* code here */};</>;
+	return (
+		<>
+			<Header temperature={temperature} />
+			<Main temperature={temperature} />
+			<Footer temperature={temperature} />
+		</>
+	);
+};
+
+const Header = ({ temperature }) => {
+	return (
+		<div>
+			<button type="button">ON / OFF</button>
+			<span>Current Temperature {temperature} Celsius</span>
+		</div>
+	);
+};
+
+const Main = ({ temperature }) => {
+	return (
+		<div>
+			<Temperature temperature={temperature} />
+		</div>
+	);
+};
+
+const Temperature = ({ temperature }) => {
+	return <div> {temperature} Celcius</div>;
+};
+
+const Footer = () => {
+	return (
+		<div>
+			<button type="button">UP</button>
+			<button type="button">DOWN</button>
+		</div>
+	);
 };
 
 export default App;
